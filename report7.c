05f9f16699f92011d0be0f92302da17cd4d7a04b
Minami UTO <e245755@ie.u-ryukyu.ac.jp>
2025-01-21 12:27:08 +0900

    2nd commit

--- report7.c
+++ report7.c
@@ -1,6 +1,29 @@
+#include <stdio.h>
+
+void sorting(int scores[], int size);
+void sorting(int scores[], int size){
+    int tiisai;
+    int s=1;
+    for(int i=0; i<size; i++){
+        for(int j=s; j<size; j++){
+            if(scores[i] < scores[j]){ //順番を交換する
+                tiisai = scores[i];
+                scores[i] = scores[j];
+                scores[j] = tiisai;
+            }
+        }
+        s++;
+    }
+
+    for(int i=0; i<size; i++){
+        printf("data[%d] = %d\n",i, scores[i]);
+    }
+}
+
 int main(){
     const int SIZE = 5; //定数として配列のサイズを宣言                          
     int scores[SIZE] = {0, 60, 70, 100, 90};
     //処理コード
+    sorting(scores, SIZE);
     return 0;
 }
