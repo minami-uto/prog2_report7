79e31e11e5cbe23fbfc3ca3f5843dadaaa6ac00e
Minami UTO <e245755@ie.u-ryukyu.ac.jp>
2025-01-21 12:32:35 +0900

    3rd commit

--- report7.c
+++ report7.c
@@ -14,16 +14,30 @@ void sorting(int scores[], int size){
         }
         s++;
     }
-
+    /*
     for(int i=0; i<size; i++){
         printf("data[%d] = %d\n",i, scores[i]);
     }
+    */
 }
 
 int main(){
     const int SIZE = 5; //定数として配列のサイズを宣言                          
     int scores[SIZE] = {0, 60, 70, 100, 90};
     //処理コード
+    printf("scores =");
+    for(int i=0; i<SIZE; i++){
+        printf(" %d", scores[i]);
+    }
+    printf("\n");
+
     sorting(scores, SIZE);
+
+    printf("results =");
+    for(int i=0; i<SIZE; i++){
+        printf(" %d", scores[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
